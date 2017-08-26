9cc4106453568a1268bfb4d3943658e5a254bf9a
nikitinskaya <nikitinskaya.v@gmail.com>
2017-08-26 17:30:32 +0300

    add error handling

--- httpclient.js
+++ httpclient.js
@@ -3,7 +3,13 @@ var http = require('http');
 var url = process.argv[2];
 
 http.get(url, function(response) {
-    response.setEncoding("utf8").on("data", function(data) {
+    response.setEncoding("utf8");
+
+    response.on("data", function(data) {
         console.log(data);
+    });
+    
+    response.on("error", function(err){
+        console.log("Got an err: ", err);
     });
 });
